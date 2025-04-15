e21d129b384f99500a55b0a527d47849e98cc5e9
marina_rodionova <marina_rodionova@>
2025-04-15 15:24:54 +0500

    KTB-02-create words file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,16 @@
 package org.example
 
-fun main() {
+import java.io.File
 
+fun main() {
+    val wordsFile: File = File("words.txt")
+    wordsFile.createNewFile()
+    wordsFile.writeText(
+        "hello привет\n" +
+                "dog собака\n" +
+                "cat кошка"
+    )
+    for (line in wordsFile.readLines()) {
+        println(line)
+    }
 }
