2db0fd4021f89949a77f65ec2894a7eaed7d65e2
marina_rodionova <marina_rodionova@>
2025-04-23 01:34:24 +0500

    KTB-08-learn words

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,7 +15,46 @@ fun main() {
         val menuAnswer = readln().toIntOrNull() ?: -1
         when (menuAnswer) {
             0 -> break
-            1 -> println("Учить слова")
+            1 -> {
+                println("Учить слова")
+                var notLearnedList = getNotLearnedList(dictionary)
+                var isContinueLearn = notLearnedList.isNotEmpty()
+
+                while (isContinueLearn) {
+                    for (notLearnedWord in notLearnedList) {
+                        println("\n${notLearnedWord.word}:")
+                        val questionWords =
+                            notLearnedList.filter { it.translate != notLearnedWord.translate }.take(3)
+                                .map { it.translate }
+                                .toMutableList()
+                        questionWords.add(notLearnedWord.translate)
+                        questionWords.shuffle()
+
+                        for (i in 0..<questionWords.size) {
+                            println("${i + 1}) - ${questionWords[i]}")
+                        }
+                        println("----------\n" + "0 - Меню")
+
+                        val userAnswerInput = readln().toIntOrNull() ?: -1
+                        val correctAnswerId = questionWords.indexOf(notLearnedWord.translate)
+
+                        if (userAnswerInput == 0) {
+                            isContinueLearn = false
+                            break
+                        } else if (userAnswerInput - 1 == correctAnswerId) {
+                            println("Правильно!")
+                            dictionary.filter { it.word == notLearnedWord.word }.forEach { it.correctAnswersCount += 1 }
+                            saveDictionary(dictionary)
+                            notLearnedList = getNotLearnedList(dictionary)
+                        } else {
+                            println("Неправильно! ${notLearnedWord.word} – ${notLearnedWord.translate}")
+                        }
+
+                    }
+
+                }
+            }
+
             2 -> {
                 println("Статистика")
                 val totalCount = dictionary.size
@@ -23,6 +62,7 @@ fun main() {
                 val percent = learnedCount / totalCount * 100
                 println("Выучено $learnedCount из $totalCount слов | $percent%")
             }
+
             else -> println("Введите число 1, 2 или 0")
         }
 
@@ -46,4 +86,20 @@ fun loadDictionary(): List<Word> {
 
 fun getLearnedCount(dictionary: List<Word>): Int {
     return dictionary.filter { word -> word.correctAnswersCount >= 3 }.count()
+}
+
+fun getNotLearnedList(dictionary: List<Word>): MutableList<Word> {
+    val notLearnedList = dictionary.filter { it.correctAnswersCount < 3 }.toMutableList()
+    if (notLearnedList.isEmpty()) {
+        println("Все слова выучены")
+    }
+    return notLearnedList
+}
+
+fun saveDictionary(dictionary: List<Word>) {
+    File("words.txt").writeText("")
+    dictionary.forEach {
+        File("words.txt").appendText("${it.word}|${it.translate}|${it.correctAnswersCount}\n")
+    }
+
 }
