28a6fe99ba5e1ee18889f05220dd1b7196f18532
marina_rodionova <marina_rodionova@>
2025-05-22 22:06:04 +0500

    KTB-12 - telegram http request

---
+++ src/main/kotlin/Telegram.kt
@@ -0,0 +1,29 @@
+package org.example
+
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+fun main(args: Array<String>){
+
+    val botToken =args[0]
+    val urlGetMe = "https://api.telegram.org/bot$botToken/getMe"
+    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates"
+
+    val client = HttpClient.newBuilder().build()
+
+    val requestGetMe = HttpRequest.newBuilder().uri(URI.create(urlGetMe)).build()
+    val requestGetUpdates = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
+
+    val responseGetMe = client.send(requestGetMe, HttpResponse.BodyHandlers.ofString())
+    val responseGetUpdates = client.send(requestGetUpdates, HttpResponse.BodyHandlers.ofString())
+
+    println(responseGetMe.body())
+    println(responseGetUpdates.body())
+
+
+
+
+
+}
